25bb4388dd74b570713eb49cd5bd65f3fa9b7de0
jsbruneau <jsbruneau@gmail.com>
2019-04-29 00:31:12 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('install', function(event) {
   console.log('SW: install Event Called');
+  fetch('http://apis.google.com/js/client.js');
 });
 self.addEventListener('fetch', function(event) {
   console.log('SW: fetch Event Called');
